9b6b417984cf09c649634c559de695f2d2b7300b
Nick Baker <45735345+nickbak@users.noreply.github.com>
2020-07-08 15:57:30 +0100

    Fix linting

--- Hudl_test.py
+++ Hudl_test.py
@@ -1,34 +1,34 @@
 import unittest
-import selenium
-import time
+
 from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class TestLogin(unittest.TestCase):
     def test_invalid_password(self):
-       
-       browser = Chrome()
 
-       try:
-           browser.implicitly_wait(2)
+        browser = Chrome()
 
-           browser.get('http://www.hudl.com/login')
+        try:
+            browser.implicitly_wait(2)
 
-           # Enter username and incorrect password
-           browser.find_element_by_id("email").send_keys("nick@threecandles.co.uk")
-           browser.find_element_by_id("password").send_keys("test")
-           browser.find_element_by_id("logIn").click()
+            browser.get('http://www.hudl.com/login')
 
-           # Expect login error text to be shown within 2 seconds
-           wait = WebDriverWait(browser, 2)
-           wait.until(EC.visibility_of(browser.find_element_by_class_name("login-error-container")), "Login error help not displayed")
-       finally:
-           browser.close()
+            # Enter username and incorrect password
+            browser.find_element_by_id("email").send_keys("nick@threecandles.co.uk")
+            browser.find_element_by_id("password").send_keys("test")
+            browser.find_element_by_id("logIn").click()
+
+            # Expect login error text to be shown within 2 seconds
+            wait = WebDriverWait(browser, 2)
+            wait.until(EC.visibility_of(browser.find_element_by_class_name("login-error-container")), "Login error help not displayed")
+        finally:
+            browser.close()
+
 
     def test_valid_login(self):
-       
+
             browser = Chrome()
 
             try:
